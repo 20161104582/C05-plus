5d4b454f31031c129dd8d6bb7ee11bc0aa10875f
20161104582 <a20161104582@scy.local>
2018-09-19 10:58:46 +0800

    plus
    Add

---
+++ C05-plus/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  C05-plus
+//
+//  Created by a20161104582 on 2018/9/14.
+//  Copyright © 2018年 a20161104582. All rights reserved.
+//
+
+import Foundation
+var sum = 100 + 100
+print(sum)
+
+print(8+8)
